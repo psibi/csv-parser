22fd59ee4589d596bb8c9ad8a6d2e2aa47ea5bf5
Sibi <psibi2000@gmail.com>
2012-09-08 01:13:51 +0530

    License headers added.

--- csv_parser.hpp
+++ csv_parser.hpp
@@ -1,3 +1,23 @@
+/*-------------------------------------------------------------------
+# Copyright (C) 2012 Sibi <sibi@psibi.in>
+#
+# This file is part of csv-parser.
+#
+# csv-parser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# csv-parser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with csv-parser.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author:   Sibi <sibi@psibi.in>
+--------------------------------------------------------------------*/
 #ifndef __CSV_PARSER_H__
 #define __CSV_PARSER_H__
 
